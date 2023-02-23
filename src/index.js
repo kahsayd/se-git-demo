3a332471d06a885545bfab1dc98b314b08ae4b9d
Adam <ajzbikowski@gmail.com>
2023-02-23 15:38:46 +0000

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.static('static'))
+// Integrate Pug with Express
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Serve assets from 'static' folder
+app.use(express.static("static"));
 
+// Landing route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Gallery route
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+
+// About route
+app.get("/about", (req, res) => {
+  res.render("about", { title: "Boring about page" });
+});
+
+// Sample API route
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+// Run server!
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
