3d01707ecfca8a65f4e45658a0bd8fa6e3f0199a
Adam Żbikowski <ajzbikowski@gmail.com>
2023-02-28 22:39:41 +0000

    Remove pug

--- src/index.js
+++ src/index.js
@@ -2,30 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Integrate Pug with Express
-app.set("view engine", "pug");
-
-// Serve assets from 'static' folder
-app.use(express.static("static"));
-
 // Landing route
 app.get("/", (req, res) => {
-  res.render("index");
-});
-
-// Gallery route
-app.get("/gallery", (req, res) => {
-  res.render("gallery");
-});
-
-// About route
-app.get("/about", (req, res) => {
-  res.render("about", { title: "Boring about page" });
+  res.send("Hello world!");
 });
 
 // Sample API route
 app.get("/ping", (req, res) => {
   res.send("pong");
+});
+
+// Returns cities from the database
+app.get("/cities", (req, res) => {
+  // TODO
 });
 
 // Run server!
